5505376e0e702e31ab9338157d1ce94433078f59
crftwoo <crftwo@gmail.com>
2026-05-15 10:35:20 -0300

    Remove pasta Catalogos do repositorio
    Remove pasta Catalogos do repositorio

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Cria pasta Catalogos no repositorio', dateIso: '2026-05-15T10:05:51-03:00' };
+const LATEST_LOG = { message: 'Remove pasta Catalogos do repositorio', dateIso: '2026-05-15T10:34:05-03:00' };
