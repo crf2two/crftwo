bcb433e0f3cfcf033bd91896e063edf601275492
Thiago Luiz da luz <thiago.luz@dufrio.com.br>
2026-06-09 19:37:32 -0300

    Corrige redeclaração da constante HYPER_BRANDS no Scraper Dufrio

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Adiciona exibição de link consultado e cópia de lista formatada para WhatsApp no Scraper Dufrio', dateIso: '2026-06-09T19:33:00-03:00' };
+const LATEST_LOG = { message: 'Corrige redeclaração da constante HYPER_BRANDS no Scraper Dufrio', dateIso: '2026-06-09T19:37:00-03:00' };
