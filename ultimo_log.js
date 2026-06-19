1d6cdfd0c35cb05a8cbcf8b1cfe4f27f7c1b6526
crf2two <crftwo@gmail.com>
2026-06-19 19:02:52 -0300

    Atualiza log da limpeza

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-window.LATEST_LOG = { message: 'Prévia do novo design do Scraper Ar criada', dateIso: '2026-06-19T18:50:00-03:00' };
+window.LATEST_LOG = { message: 'Limpeza de arquivos do redesign recusado', dateIso: '2026-06-19T19:02:00-03:00' };
