0cdf239eaffbe85e1415c2223a423ba96bd365c7
crf2two <crftwo@gmail.com>
2026-06-24 20:28:26 -0300

    Atualiza log do seletor

--- ultimo_log_override.js
+++ ultimo_log_override.js
@@ -1,1 +1,1 @@
-window.LATEST_LOG = { message: 'Campos do Seletor ajustados', dateIso: '2026-06-24T20:18:08-03:00' };
+window.LATEST_LOG = { message: 'Perfil automatico e ambiente 35 no seletor', dateIso: '2026-06-24T20:24:00-03:00' };
